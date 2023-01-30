0f3ff121392399bb4b397a5532c76e67254b742c
labmaximo <123917340+labmaximo@users.noreply.github.com>
2023-01-30 14:32:07 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,14 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	// alert("ok");
+
+
+	let nombre;
+	nombre = prompt ("ingrese su nombre") ;
+
+	nombre = "maximo" ; //asignacion de string
+    
+	alert (nombre);
 }
 
